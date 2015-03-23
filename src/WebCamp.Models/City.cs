5a0b7be469a8a78af693adcd4bdecc238be1295e
Christian Rygg <Christian.Rygg@indit.no>
2015-03-23 20:51:18 +0100

    Added City Model

---
+++ src/WebCamp.Models/City.cs
@@ -0,0 +1,13 @@
+﻿namespace WebCamp.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCapital { get; set; }
+        public int Polulation { get; set; }
+        public int FoundedYear { get; set; }
+        public string IconUrl { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
