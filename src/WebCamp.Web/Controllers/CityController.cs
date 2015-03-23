0d2b7dfe66efa4602f1d05826c8a7cce4a8aeb35
Christian Rygg <Christian.Rygg@indit.no>
2015-03-23 21:15:37 +0100

    Added CityController

---
+++ src/WebCamp.Web/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebCamp.Models;
+
+namespace WebCamp.Web.Controllers
+{
+    public class CityController : Controller
+    {
+        // GET: City
+        public ActionResult Index()
+        {
+            // TODO - get model
+            var cities = new List<City>();
+            return View(cities);
+        }
+
+        [ActionName("Index")]
+        public ActionResult Index(int id)
+        {
+            // TODO - get model
+            var city = new City();
+            return View("CityDetails", city);
+        }
+    }
+}
